5c26379ca83d8939a4222873a22645c6d5956586
puffi <puffi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-31 20:47:10 +0000

    loading of the right file class.tx_tidier_crawler.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tidier/trunk@46055 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,5 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 // Register with "crawler" extension:
 $TYPO3_CONF_VARS['EXTCONF']['crawler']['procInstructions']['tx_tidier_tidy'] = 'Check HTML with Tidy';
-$TYPO3_CONF_VARS['EXTCONF']['crawler']['cli_hooks']['tx_tidier_crawl'] = 'EXT:tidier/class.crawler.php:&tx_tidier_crawler';
+$TYPO3_CONF_VARS['EXTCONF']['crawler']['cli_hooks']['tx_tidier_crawl'] = 'EXT:tidier/class.tx_tidier_crawler.php:&tx_tidier_crawler';
 ?>
