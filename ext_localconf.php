6cd532b40152fe652b185ec0ee98d3fb4846e29b
puffi <puffi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-01 21:36:34 +0000

    tasks init for scheduler
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tidier/trunk@46133 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,12 @@ if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 // Register with "crawler" extension:
 $TYPO3_CONF_VARS['EXTCONF']['crawler']['procInstructions']['tx_tidier_tidy'] = 'Check HTML with Tidy';
 $TYPO3_CONF_VARS['EXTCONF']['crawler']['cli_hooks']['tx_tidier_crawl'] = 'EXT:tidier/class.tx_tidier_crawler.php:&tx_tidier_crawler';
+
+// Add task to scheduler
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_tidier_task'] = array(
+    'extension'        => $_EXTKEY,
+    'title'            => 'Tidier task',
+    'description'      => 'Check with the scheduler the quality of your HTML code.',
+    'additionalFields' => 'tx_tidier_task_addFields',
+);
 ?>
