602b6911bb063777ab01bee61ded5d7f8e502b60
gerscht <gerscht@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-01 20:48:06 +0000

    Database structure
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tidier/trunk@46128 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,6 +1,20 @@
-#
-# Table structure for table 'pages'
-#
-CREATE TABLE pages (
-	tx_tidier_tidy_errors tinytext
+
+
+CREATE TABLE tx_tidier_error (
+	uid int(11) NOT NULL auto_increment,
+	error_pid int(11) DEFAULT '0' NOT NULL,
+	error_code_uid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	error_line mediumint(8) UNSIGNED DEFAULT '0' NOT NULL,
+	error_column mediumint(8) UNSIGNED DEFAULT '0' NOT NULL,
+	PRIMARY KEY (uid)
+);
+
+CREATE TABLE tx_tidier_errorcode (
+	uid int(11) NOT NULL auto_increment,
+	error_type varchar(20) DEFAULT '' NOT NULL,
+	error_code varchar(255) DEFAULT '' NOT NULL,
+	error_priority tinyint(3) UNSIGNED DEFAULT '0' NOT NULL,
+	error_text text,
+	PRIMARY KEY (uid)
 );
