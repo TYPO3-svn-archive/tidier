075ff3484326f782bbcd21a561a9dbd6186f9fe3
gerscht <gerscht@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-01 21:47:30 +0000

    Using localconf instead of ext_autoload.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tidier/trunk@46135 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-$extensionPath = t3lib_extMgm::extPath('tidier');
-
-return array(
-	'tx_tidier_processor' 		=> $extensionPath.'classes/class.tx_tidier_processor.php',
-	'tx_tidier_task' 			=> $extensionPath.'tasks/class.tx_tidier_task.php',
-	'tx_tidier_task_addFields' 	=> $extensionPath.'tasks/class.tx_tidier_task_addFields.php',
-);
-?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,14 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
+
+// possibly bug, that it doesn't use ext_autoload.php
+if(TYPO3_MODE != 'FE'){
+	require_once(t3lib_extMgm::extPath('tidier').'tasks/class.tx_tidier_task.php');	
+	require_once(t3lib_extMgm::extPath('tidier').'tasks/class.tx_tidier_task_addFields.php');	
+}
+
+
 // Register with "crawler" extension:
 $TYPO3_CONF_VARS['EXTCONF']['crawler']['procInstructions']['tx_tidier_tidy'] = 'Check HTML with Tidy';
 $TYPO3_CONF_VARS['EXTCONF']['crawler']['cli_hooks']['tx_tidier_crawl'] = 'EXT:tidier/class.tx_tidier_crawler.php:&tx_tidier_crawler';
