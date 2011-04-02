917a89babf67fc4bae7d0acb35964c9d3b5734c6
puffi <puffi@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-02 07:29:01 +0000

    add missing fields
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tidier/trunk@46142 735d13b6-9817-0410-8766-e36946ffe9aa

--- tasks/class.tx_tidier_task_addFields.php
+++ tasks/class.tx_tidier_task_addFields.php
@@ -1,55 +1,111 @@
 <?php
 class tx_tidier_task_addFields implements tx_scheduler_AdditionalFieldProvider {
-	
+    
 	public function getAdditionalFields(array &$taskInfo, $task, tx_scheduler_Module $parentObject) {
+
+		if (empty($taskInfo['tidy_domain'])) {
+            if($parentObject->CMD == 'edit') {
+                $taskInfo['tidy_domain'] = $task->tidy_domain;
+            } else {
+                $taskInfo['tidy_domain'] = t3lib_div::getIndpEnv('TYPO3_REQUEST_HOST').'/';
+            }
+        }
+		
+        if (empty($taskInfo['tidy_pid'])) {
+            if($parentObject->CMD == 'edit') {
+                $taskInfo['tidy_pid'] = $task->tidy_pid;
+            } else {
+                $taskInfo['tidy_pid'] = '';
+            }
+        }
+        
+		if (empty($taskInfo['tidy_selectedBEUsers'])) {
+			$taskInfo['tidy_selectedBEUsers'] = array();
+			if ($parentObject->CMD == 'edit') {
+					// In case of editing the task, set to currently selected value
+				$taskInfo['tidy_selectedBEUsers'] = $task->tidy_selectedBEUsers;
+			}
+		}
+        
+        $additionalFields = array();
+        
+        // Write the code for the field
+        $fieldID = 'tidy_domain';
+        $fieldCode = '<input type="text" name="tx_scheduler['. $fieldID .']" id="' . $fieldID . '" value="' . $taskInfo['tidy_domain'] . '" size="30" />';
+        $additionalFields[$fieldID] = array(
+            'code'     => $fieldCode,
+            'label'    => 'Domain to tidy'
+        );
+		
+		$fieldID = 'tidy_pid';
+        $fieldCode = '<input type="text" name="tx_scheduler['. $fieldID .']" id="' . $fieldID . '" value="' . $taskInfo['tidy_pid'] . '" size="30" />';
+        $additionalFields[$fieldID] = array(
+            'code'     => $fieldCode,
+            'label'    => 'Page id to tidy'
+        );
+
+        // Write the code for the field
+        $fieldID = 'tidy_selectedBEUsers';
+        //$fieldCode = '<input type="text" name="tx_scheduler['. $fieldID .']" id="' . $fieldID . '" value="' . $taskInfo['tidy_email_to'] . '" size="30" />';
+        $fieldOptions = $this->getBEUsersOptions($taskInfo['tidy_selectedBEUsers']);
+        $fieldHtml =
+			'<select name="tx_scheduler['. $fieldID .'][]" id="' . $fieldId . '" class="wide" size="10" multiple="multiple">' .
+				$fieldOptions .
+			'</select>';
+				
+        $additionalFields[$fieldID] = array(
+            'code'     => $fieldHtml,
+            'label'    => 'Email to send to'
+        );
+        
+        return $additionalFields;
+    }
+
+    public function validateAdditionalFields(array &$submittedData, tx_scheduler_Module $parentObject) {
+		$submittedData['tidy_domain'] = trim($submittedData['tidy_domain']);
+		$submittedData['tidy_pid'] = trim($submittedData['tidy_pid']);
+		$submittedData['tidy_selectedBEUsers'] = $submittedData['tidy_selectedBEUsers'];
+        //$submittedData['tidy_email_to'] = trim($submittedData['tidy_email_to']);
+        return true;
+    }
+
+    public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task) {
+        $task->tidy_domain = $submittedData['tidy_domain'];
+        $task->tidy_pid = $submittedData['tidy_pid'];
+        $task->tidy_selectedBEUsers = $submittedData['tidy_selectedBEUsers'];
+    }
+    
+    protected function getBEUsersOptions(array $tidy_selectedBEUsers) {
+		$options = array();
+		$BEUsers = $this->getBEUsers();
+		foreach ($BEUsers as $BEUser) {
+			if (in_array($BEUser['uid'], $tidy_selectedBEUsers)) {
+				$selected = ' selected="selected"';
+			} else {
+				$selected = '';
+			}
+			$options[] =
+				'<option value="'.$BEUser['uid'].'"'.$selected . '>' .
+					$BEUser['username'].' - '.$BEUser['realName'].' - '.$BEUser['email'].
+				'</option>';
+		}
+		return implode($options);
+	}
 	
-		if (empty($taskInfo['ip'])) {
-			if($parentObject->CMD == 'edit') {
-				$taskInfo['ip'] = $task->ip;
-			} else {
-				$taskInfo['ip'] = '';
-			}
-		}
-
-		if (empty($taskInfo['port'])) {
-			if($parentObject->CMD == 'add') {
-				$taskInfo['port'] = '80';
-			} elseif($parentObject->CMD == 'edit') {
-				$taskInfo['port'] = $task->port;
-			} else {
-				$taskInfo['port'] = '';
-			}
-		}
-
-		// Write the code for the field
-		$fieldID = 'task_ip';
-		$fieldCode = '<input type="text" name="tx_scheduler[ip]" id="' . $fieldID . '" value="' . $taskInfo['ip'] . '" size="30" />';
-		$additionalFields = array();
-		$additionalFields[$fieldID] = array(
-			'code'	 => $fieldCode,
-			'label'	=> 'IP-Adresse/Webseite'
+	protected function getBEUsers() {
+		$records = array();
+		$result = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+			'*',
+			'be_users',
+			"username NOT LIKE '_cli%' AND email !='' AND disable=0 AND deleted=0"
 		);
 
-		// Write the code for the field
-		$fieldID = 'task_port';
-		$fieldCode = '<input type="text" name="tx_scheduler[port]" id="' . $fieldID . '" value="' . $taskInfo['port'] . '" size="30" />';
-		$additionalFields[$fieldID] = array(
-			'code'	 => $fieldCode,
-			'label'	=> 'Port'
-		);
+		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($result)) {
+			 $records[] = $row;
+		} 
+		$GLOBALS['TYPO3_DB']->sql_free_result($result);
 
-		return $additionalFields;
-	}
-
-	public function validateAdditionalFields(array &$submittedData, tx_scheduler_Module $parentObject) {
-		$submittedData['ip'] = trim($submittedData['ip']);
-		$submittedData['port'] = trim($submittedData['port']);
-		return true;
-	}
-
-	public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task) {
-		$task->ip = $submittedData['ip'];
-		$task->port = $submittedData['port'];
+		return $records;		
 	}
 }
 ?>
